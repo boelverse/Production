7c35f59f1fe228d19a1ccd389a20af72a1a3126b
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-01-13 22:16:14 +0000

    Deploy Flutter Web App (Version: )

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "8807e432866770d4a59c69be18ae685d",
+"main.dart.js": "77a10b1e6c832bb8ac9046c77f6d9a9a",
 "splash/img/light-3x.png": "8252990d7229d3965b2ab0ae85bc4f02",
 "splash/img/light-1x.png": "af5ab671d6f3d0df916fc34d9d7a8bef",
 "splash/img/light-2x.png": "c209435123ef6245a6c5f72b57ad755a",
@@ -46,8 +46,8 @@ const RESOURCES = {"flutter.js": "f393d3c16b631f36852323de8e583132",
 "icons/Icon-72.png": "b01f313c0225573dc40498e4ec262d8b",
 "icons/Icon-96.png": "7b678632e098a409e3ef73cc0fb2b04e",
 "favicon.png": "7f75a2244b29632fd384b215579c878a",
-"version.json": "5665d871127352dd61e8c962f4f62db3",
-"flutter_bootstrap.js": "c348a4c04b9234b961788c909ceecb5d"};
+"version.json": "60b54cba08efb5cc4f2d11fd2b9cfc50",
+"flutter_bootstrap.js": "7bbe390827acc584eb7e041ccf63d599"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
