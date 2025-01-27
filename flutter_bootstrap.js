02df46d44fcf021500ec273ef3c854c7ea123a3b
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-01-27 21:43:04 +0000

    Deploy Flutter Web App (Version: 1.6.0)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "26722992"
+    serviceWorkerVersion: "2985756809"
   }
 });
