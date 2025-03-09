22a948e4c670a070c376c820bbc7d26261a62640
MichaelBielenPXL <michael.bielen@student.pxl.be>
2025-03-09 12:19:54 +0000

    Deploy Flutter Web App (patch version bump: 0.8.24)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,9 +36,9 @@ const RESOURCES = {"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "assets/AssetManifest.bin.json": "4e3b814f647fcd78d79f85f5e63aebe8",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b89682a9e4402af142ce2b40cd2c98b0",
 "assets/AssetManifest.json": "6fa9336d041ca5770da6e1cfb97d87cc",
-"version.json": "06c6b8d96d8c51990a95d75a2585de6c",
+"version.json": "cdb3bb4cc052380a2bff37e85ab154ff",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "4726a6a4a03e47a1010fe2a0a699eb6c",
+"main.dart.js": "ebbd588634bc93cc0b2ff7ae244c0b24",
 "firebase-messaging-sw.js": "d11bd10961c5a6fffe968d8533fd90c1",
 "icons/Icon-96.png": "7b678632e098a409e3ef73cc0fb2b04e",
 "icons/Icon-maskable-512.png": "72f0ddf85c0feb650daf4a58422fd399",
@@ -49,7 +49,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
 "icons/Icon-maskable-192.png": "da3a393e1c3c8ace9202e53822af578d",
 "manifest.json": "16796f2cec421f5e50f48fb01663bad3",
 "favicon.png": "7f75a2244b29632fd384b215579c878a",
-"flutter_bootstrap.js": "9680835e34cdccc876bd8410ba59858d"};
+"flutter_bootstrap.js": "0be55f6c50fc528185c431905c48e244"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
